002e3784f15e4879e3ac8d4e5b12613568edf302
Ayanavo <ayanavo2057@gmail.com>
2024-09-02 23:51:50 +0530

    sync export package

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import "@ayanavo/locusjs";
+// eslint-disable-next-line no-unused-vars
+import locus from "@ayanavo/locusjs";
 const numbers = [1, 2, 3, 4, 5];
-console.log(numbers.avj());
+console.log(numbers.empty());
